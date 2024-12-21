07e3d6ce6d9496c945cb6629dbd7681c66f5c132
Azizbek Rahkmanaliyev <zizich@mail.ru>
2024-12-22 01:11:51 +0500

    catalog.html, views.py переделали пагинацию под фильтр

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,3 +1,5 @@
+from django.utils.http import urlencode
+
 from goods.models import Categories
 from django import template
 
@@ -7,3 +9,10 @@ register = template.Library()
 @register.simple_tag()
 def categories_tags():
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)  # все контекстные переменные попадут в функцию, которые передаются с views
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
