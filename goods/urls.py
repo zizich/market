3b697fbf657d934e07166b7e9371510d9b228dee
Azizbek Rahkmanaliyev <zizich@mail.ru>
2024-12-22 01:31:45 +0500

    работа над поисковиком. Создан отдельный файл в goods/utils.py где есть функция по поиску. В goods/views.py в модели выполнен код по получени информации по запросу query

--- goods/urls.py
+++ goods/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "goods"
 
 urlpatterns = [
+    path('search/', views.catalog, name='search'),
     path('<slug:category_slug>/', views.catalog, name='index'),
-    path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
---
+++ goods/utils.py
@@ -0,0 +1,6 @@
+from goods.models import Products
+
+
+def q_search(query):
+    if query.isdigit() and len(query) <= 5:
+        return Products.objects.filter(id=int(query))
