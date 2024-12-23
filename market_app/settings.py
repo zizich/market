79cbdecc849f88bd0c108cfef4cb3fc11dc8e8d4
Azizbek Rahkmanaliyev <zizich@mail.ru>
2024-12-24 00:19:47 +0500

    создание нового приложение users и для него templates

--- market_app/settings.py
+++ market_app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users',
 ]
 
 MIDDLEWARE = [
---
+++ users/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+
+def login(request):
+    context = {
+        'title': 'Home - Авторизация'
+    }
+
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    context = {
+        'title': 'Home - Регистрация'
+    }
+
+    return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    context = {
+        'title': 'Home - Профиль'
+    }
+
+    return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    context = {
+        'title': 'Home - Выход'
+    }
+
+    return render(request, 'users/login.html', context)
