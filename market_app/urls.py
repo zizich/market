ff6ebf5779e23db38ac653705cf24b821428d255
Azizbek Rahkmanaliyev <zizich@mail.ru>
2024-12-24 13:48:33 +0500

    Добавил templates users. Изменение ссылок в личный кабинет, профиль, регистрацию в base.html

--- market_app/urls.py
+++ market_app/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalog/', include("goods.urls", namespace='catalog')),
-    path('user/', include('users/urls.py'), namespace='user')
+    path('user/', include('users.urls', namespace='user')),
 ]
 
 if settings.DEBUG:
