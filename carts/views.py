7e34e3d3183f166fe5ba30132d1ffd6550545869
Azizbek Rahkmanaliyev <zizich@mail.ru>
2025-01-05 16:46:04 +0500

    get_user_carts

--- carts/views.py
+++ carts/views.py
@@ -1,13 +1,18 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
 from carts.models import Cart
+from carts.utils import get_user_carts
 from goods.models import Products
 
 
 # добавление в корзину
-def cart_add(request, product_slug):
+def cart_add(request):
 
-    product = Products.objects.get(slug=product_slug)
+    product_id = request.POST.get('product_id')
+
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -21,7 +26,17 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META['HTTP_REFERER'])  # возврат на предыдущую страницу
+    user_cart = get_user_carts(request)
+
+    cart_items_html = render_to_string('carts/includes/included_cart.html',
+                                       {'carts': user_cart},
+                                       request=request)
+    response_data = {"message": 'Товар добавлен в корзину',
+                     'cart_items_html': cart_items_html}
+
+    return JsonResponse(response_data)
+
+    # return redirect(request.META['HTTP_REFERER'])  # возврат на предыдущую страницу
 
 
 # изменить количества товаров в корзине
