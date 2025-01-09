cb32a2f95bc17f63a8677193bf09e6bb7f828ce8
Azizbek Rahkmanaliyev <zizich@mail.ru>
2025-01-09 11:28:46 +0500

    Закончили с корзиной, а именно добавление товара, изменение количество

--- carts/views.py
+++ carts/views.py
@@ -42,12 +42,51 @@ def cart_add(request):
 
 
 # изменить количества товаров в корзине
-def cart_change(request, product_slug):
-    ...
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request)
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
 
 
 # удалить корзину
-def cart_remove(request, cart_id):
+def cart_remove(request):
+
+    cart_id = request.POST.get("cart_id")
+
     cart = Cart.objects.get(id=cart_id)
+
+    quantity = cart.quantity
+
     cart.delete()
-    return redirect(request.META['HTTP_REFERER'])  # возврат на предыдущую страницу
+
+    user_cart = get_user_carts(request)
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+
+    return JsonResponse(response_data)
