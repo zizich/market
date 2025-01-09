70a29efa5d4cb338b8e51b1b468e6b62d2e74622
Azizbek Rahkmanaliyev <zizich@mail.ru>
2025-01-09 12:02:29 +0500

    Сессия для анонимного пользователя

--- carts/views.py
+++ carts/views.py
@@ -26,6 +26,17 @@ def cart_add(request):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        carts = Cart.objects.filter(session_key=request.session.session_key, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(session_key=request.session.session_key, product=product, quantity=1)
 
     user_cart = get_user_carts(request)
 
