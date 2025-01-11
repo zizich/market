469d504806446604250a7754bf3f8f3cd6692bda
Azizbek Rahkmanaliyev <zizich@mail.ru>
2025-01-11 17:14:00 +0500

    перевод контроллеров с функции на классы

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path  # сопоставляет запросы пользователя с функциями их обработки
-from . import views
+from .views import IndexView, AboutView
 
 app_name = "main"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', IndexView.as_view(), name='index'),
+    path('about/', AboutView.as_view(), name='about'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,22 +1,43 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
-from goods.models import Categories
-
-
-def index(request):
-
-    context = {
-        'title': 'Home - Главная страница',
-        'content': 'Магазин мебели HOME',
-    }
-    return render(request, 'main/index.html', context)
+from django.views.generic import TemplateView
 
 
-def about(request):
-    context = {
-        'title': 'Home - О компании',
-        'content': 'О компании',
-        'text_on_page': 'Текст о том почему этот магазин такой классный',
-    }
-    return render(request, 'main/about.html', context)
+class IndexView(TemplateView):
+    template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Главная страница'
+        context['content'] = 'Магазин мебели HOME'
+
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = "main/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - О компании'
+        context['content'] = 'О компании'
+        context['text_on_page'] = 'Текст о том почему этот магазин такой классный'
+
+        return context
+
+
+# def index(request):
+#
+#     context = {
+#         'title': 'Home - Главная страница',
+#         'content': 'Магазин мебели HOME',
+#     }
+#     return render(request, 'main/index.html', context)
+
+
+# def about(request):
+#     context = {
+#         'title': 'Home - О компании',
+#         'content': 'О компании',
+#         'text_on_page': 'Текст о том почему этот магазин такой классный',
+#     }
+#     return render(request, 'main/about.html', context)
